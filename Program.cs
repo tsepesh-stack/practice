d7c5f09c19c5f085505f18f929fc48d9bcd6c82c
tsepesh-stack <tsepeshww@gmail.com>
2026-04-16 22:39:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+namespace Training1
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Создаем объект класса Person и заполняем его данными
+            // Person person = new Person();
+            // person.name = Console.ReadLine();
+            // person.age = int.Parse(Console.ReadLine());
+            // person.Print();
+
+
+            // Создаем объект класса Car и заполняем его данными
+            // Car car1 = new Car();
+            // car1.brand = Console.ReadLine(); 
+            // car1.model = Console.ReadLine();
+            // car1.year = int.Parse(Console.ReadLine());
+            // car1.PrintInfo();
+
+            // Car car2 = new Car();
+            // car2.brand = Console.ReadLine(); 
+            // car2.model = Console.ReadLine();
+            // car2.year = int.Parse(Console.ReadLine());
+            // car2.PrintInfo();
+
+
+            // Внедренние массива объектов класса Book
+            // Book[] books = new Book[3];
+            // books[0]=new Book();
+            // books[0].title = "Война и мир";
+            // books[0].author = "Лев Толстой";
+            // books[1]=new Book();
+            // books[1].title = "Преступление и наказание";
+            // books[1].author = "Федор Достоевский";
+            // books[2]=new Book();
+            // books[2].title = "Мастер и Маргарита";
+            // books[2].author = "Михаил Булгаков";
+            // for (int i = 0; i < books.Length; i++)
+            // {
+            //     books[i].Show();
+            // }
+
+
+            // Создаем объекты класса Student и выводим информацию
+            // Student Ann = new Student("Alice", 85);
+            // Ann.PrintInfo();
+            // Student Bob = new Student("Bob", 90);
+            // Bob.PrintInfo();
+            // Student Charlie = new Student("Charlie", 78);
+            // Charlie.PrintInfo();
+            // Student.PrintTotal();
+
+            // Car создание авто и подсчет авто
+            // Car ford = new Car("mustang", 2019);
+            // ford.Print();
+            // Car cadilac = new Car("escolate", 2019);
+            // cadilac.Print();
+            // Car shevrole = new Car("camaro", 2019);
+            // shevrole.Print();
+            // Car.PrintTotal();
+
+            // Product создание и подсчет продуктов 
+            // Product mango = new Product("манго", 192);
+            // mango.Print();
+            // Product banana = new Product("банан", 52);
+            // banana.Print();
+            // Product.Printtotal();
+
+            // Employee для закрепления тоже самое
+            // Employee work1 = new Employee("Jonh", 2.3);
+            // work1.Print();
+            // Employee work2 = new Employee("Bob", 5.2);
+            // work2.Print();
+            // Employee.PrintTotal();
+
+            // Smartphone для закрепления
+            // Smartphone phone1 = new Smartphone("Iphone 15","iOS 17",128);
+            // phone1.Print();
+            // Smartphone phone2 = new Smartphone("Samsung S24","Android 14", 256);
+            // phone2.Print();
+            // Smartphone phone3 = new Smartphone("Xiaomi 14","HyperOS", 512);
+            // phone3.Print();
+            // Smartphone.printTotal();
+
+            // Book опять повторение
+            // Book book1 = new Book("Война и мир", "Толстой", 1869);
+            // book1.Print();
+            // Book book2 = new Book("Мастер и Маргарита","Булгаков");
+            // book2.Print();
+            // Book book3 = new Book();
+            // book3.Print();
+            // Book book4 = new Book("Азы програмированния","Столяров",2019);
+            // book4.Print();
+            // Book.TotalPrint();
+
+            // Item использование this в полной мере
+            Item item1 = new Item("back", 323.43);
+            item1.Print();
+            Item item2 = new Item("table");
+            item2.Print();
+            Item item3 = new Item();
+            item3.Print();
+            Item.PrintInfo();
+
+            
+            
+            
+        }
+    }
+}
